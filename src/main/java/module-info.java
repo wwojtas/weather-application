5aa949e98c81b90d5fa6e5cf96645d758c74cd34
wwojtas <wojciech.wus.programista@gmail.com>
2022-03-31 21:55:27 +0200

    First step with weather app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module weather {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+    requires java.desktop;
+
+    opens pl.weather;
+}
---
+++ src/main/java/pl/weather/Main.java
@@ -0,0 +1,19 @@
+package pl.weather;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        primaryStage.setTitle("Pogoda");
+        primaryStage.show();
+
+    }
+}
